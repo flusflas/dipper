927d09ba628dc13ca0051682f4ce5277bc4206eb
Flus Flas <aflusflas@gmail.com>
2022-11-13 02:04:14 +0100

    refactor: move IsFieldError() tests to error_test.go

--- godotted_test.go
+++ godotted_test.go
@@ -1,7 +1,6 @@
 package godotted_test
 
 import (
-	"fmt"
 	"godotted"
 	"reflect"
 	"testing"
@@ -366,42 +365,6 @@ func TestGetAttributes(t *testing.T) {
 			got := godotted.GetAttributes(tt.args.v, tt.args.attributes)
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetAttributes() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestIsFieldError(t *testing.T) {
-	tests := []struct {
-		name string
-		arg  interface{}
-		want bool
-	}{
-		{
-			name: "nil interface",
-			arg:  nil,
-			want: false,
-		},
-		{
-			name: "no error value",
-			arg:  123,
-			want: false,
-		},
-		{
-			name: "non-field error",
-			arg:  fmt.Errorf("some error"),
-			want: false,
-		},
-		{
-			name: "field error",
-			arg:  godotted.ErrNotFound,
-			want: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := godotted.IsFieldError(tt.arg); got != tt.want {
-				t.Errorf("IsFieldError() = %v, want %v", got, tt.want)
 			}
 		})
 	}
